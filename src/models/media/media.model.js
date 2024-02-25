d10a350266aafebd11e842bc94fcc82273f5bb4f
Eddie Sosera <21100419@virtualwindow.co.za>
2024-02-26 01:40:35 +0200

    Media Model: Color Format Validation

--- src/models/media/media.model.js
+++ src/models/media/media.model.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 const { toJSON, paginate } = require('../plugins');
+const { types } = require('joi');
+
+let hex = /^#([0-9a-fA-F]{6})$/;
+let hsla = /^hsla\((\d+),\s*(\d+%)\s*,\s*(\d+%)\s*,\s*([01](?:\.\d+)?)\)$/;
+let hsba = /^hsba\((\d+),\s*(\d+%)\s*,\s*(\d+%)\s*,\s*([01](?:\.\d+)?)\)$/;
+let rgba = /^rgba\((\d+),\s*(\d+),\s*(\d+),\s*([01](?:\.\d+)?)\)$/;
+
+const colorFormatValidation = (value) => {
+    if (
+        !value.match(hex) || // Hex: #ffffff
+        !value.match(hsla) || // HSLA: hsla(0, 0%, 7%, 1)
+        !value.match(hsba) || //HDBA: hsba(0, 0%, 7%, 1)
+        !value.match(rgba) //RGBA: rgba(18, 18, 18, 1)
+    ) {
+        throw new Error('Color must be in a valid format');
+    }
+}
 
 const mediaSchema = mongoose.Schema(
     {
@@ -41,6 +58,14 @@ const mediaSchema = mongoose.Schema(
                 default: '',
                 enum: ["1:1", "2:1", "4:3", "14:9", "16:10", "21:9"]
             },
+        },
+        colorScheme: {
+            accent: {
+                type: String,
+                validate(value) {
+                    colorFormatValidation(value)
+                }
+            }
         },
         uploadedOn: {
             type: Date,
